61fd6eaba432af8298774d5d8588e1c04baded89
MoonBreakerz <gabrielguedesdesouza@gmail.com>
2023-05-05 20:30:59 -0300

    Retirada
    Retirada de scripts para teste

--- vuelaravel.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE vuelaravel;
-USE vuelaravel;
-
-CREATE TABLE list_tarefas (
-    id INT(11) NOT NULL AUTO_INCREMENT,
-    descricao VARCHAR(255) NOT NULL,
-    finalizado ENUM('sim', 'não') NOT NULL DEFAULT 'não',
-    PRIMARY KEY (id)
-);
